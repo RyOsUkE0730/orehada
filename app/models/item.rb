a7f377463e810aa650a8b58c25f691ad464dd8d2
RyOsUkE <ryosukek74@gmail.com>
2022-02-22 16:38:55 +0900

    Setting association

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :genre
+  belongs_to :user
+  has_one_attached :image
 
   with_options presence: true do
     validates :title
