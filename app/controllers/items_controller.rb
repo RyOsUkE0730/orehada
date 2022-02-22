d8f8c999be5c9f7b9ae095d8352c59092c8c0b35
RyOsUkE <ryosukek74@gmail.com>
2022-02-22 16:44:49 +0900

    Create create action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,16 +3,33 @@ class ItemsController < ApplicationController
 
 
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
   private
   def move_to_index
     unless user_signed_in?
       redirect_to action: :index
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :description, :genre_id, :image)
+    .merge(user_id: current_user.id)
   end
 end
