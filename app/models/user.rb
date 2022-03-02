55f90878997145609b97f323f22e11030365fd1b
RyOsUkE <ryosukek74@gmail.com>
2022-03-02 20:24:23 +0900

    Setting association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
+  has_many :items, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_items, through: :likes, source: :item
+
+
+  def already_liked?(item)
+    self.likes.exists?(item_id: item.id)
+  end
 end
