a31ef832fe1ef911ae1dfa095e3bad8c43ae047e
RyOsUkE <ryosukek74@gmail.com>
2022-03-02 20:24:50 +0900

    Setting controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
   def user_params
     params.require(:user).permit(:nickname, :email, :avatar)
